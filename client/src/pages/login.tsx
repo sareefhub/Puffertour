b1dfa283a737790cf01cd6fc44a308094816b650
โซล่า ไพบูลย์สมบัติ <solask2546@gmail.com>
2023-02-21 23:31:40 +0700

    add background login

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 import "./login.css";
 
@@ -7,12 +7,16 @@ const Login: React.FC = (props) => {
   const navigate = useNavigate();
 
   return (
-    <form className="form">
-      <p className="heading">เข้าสู่ระบบ</p>
-      <input className="input" placeholder="อีเมล" type="text" />
-      <input className="input" placeholder="รหัสผ่าน" type="text" /> 
-      <button className="btn">เข้าสู่ระบบ</button>
-    </form>
+    <div className="container">
+      <div className="image">
+        <form className="form">
+          <p className="heading">เข้าสู่ระบบ</p>
+          <input className="input" placeholder="อีเมล" type="text" />
+          <input className="input" placeholder="รหัสผ่าน" type="text" />
+          <button className="btn">เข้าสู่ระบบ</button>
+        </form>
+      </div>
+    </div>
   );
 };
 
