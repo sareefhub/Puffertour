30fb3e0454636e223f5becbeb718937b933d790a
โซล่า ไพบูลย์สมบัติ <solask2546@gmail.com>
2023-02-26 22:37:07 +0700

    fix cardtour from vertical to horizontal

--- client/src/pages/days-tour.tsx
+++ client/src/pages/days-tour.tsx
@@ -7,7 +7,6 @@ import { useState, useEffect } from "react";
 import { Grid } from '@mui/material';
 import Repository from '../repositories';
 
-
 const DaysTour = () => {
   const [DataTour, setDataTour] = useState<OneDayTour[]>([]);
   const fetchData = async () => {
@@ -23,20 +22,20 @@ const DaysTour = () => {
 
   return (
     <div className="daystour-container" >
-        <Navbar />
-        <Picture />
+      <Navbar />
+      <Picture />
       <div className="tap-bar">
-          <span className="text-tour">ทัวร์รายวัน</span>
+        <span className="text-tour">ทัวร์รายวัน</span>
       </div>
-        {DataTour.map((item, index) =>
-        <Grid item xs={2} sm={4} md={4} lg={3} xl={2} key={index}>
-          <Card Tour={item}/>
-        </Grid>
-      )}
+      <Grid container spacing={2} justifyContent="center">
+        {DataTour.map((item, index) => (
+          <Grid item key={index}>
+            <Card Tour={item}/>
+          </Grid>
+        ))}
+      </Grid>
     </div>
   )
-  }
-
-
+}
 
 export default DaysTour;
