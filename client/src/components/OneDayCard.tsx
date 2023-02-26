9e3a9eadd679655d5b5a3212e486ea10f44416e9
psu6510110425 <6510110425@email.psu.ac.th>
2023-02-26 08:34:17 +0700

    แก้ไขcard

--- client/src/components/OneDayCard.tsx
+++ client/src/components/OneDayCard.tsx
@@ -1,6 +1,6 @@
 import OneDayTour from '../models/OneDay'
 import { useNavigate } from "react-router-dom"
-
+import "./Cardtour.css"
 
 interface Props {
     Tour: OneDayTour;
@@ -12,21 +12,18 @@ function OneDayCard(props: Props) {
     const img = `http://localhost:1337${tour.cover_image.data.attributes.formats.thumbnail.url}`
 
     return (
-        <div>
-        <div>{tour.name}</div>
-        <div>
-          {img}
+        <div className="card">
+            <div className="title-card">{tour.name}</div>
+                <div className="card-image">{img}</div>
+                <div className="heading">{tour.description}
+                <div className="see-more">
+                    <button onClick={() => navigate(`/additional-information-days-tour/${props.Tour.id}`)}>
+                        ดูเพิ่มเติม
+                    </button>
+                </div>
+                <div className="author">เหลือเพียง {tour.remaining} ที่ เท่านั้น!!!</div>
+            </div>
         </div>
-        <div>
-            {tour.description}
-        <div className="see-more">
-          <button onClick={() => navigate(`/additional-information-days-tour/${props.Tour.id}`)}>
-            ดูเพิ่มเติม
-          </button>
-        </div>
-        <div>เหลือเพียง {tour.remaining} ที่ เท่านั้น!!!</div>
-        </div>
-      </div>
     )
 }
 
