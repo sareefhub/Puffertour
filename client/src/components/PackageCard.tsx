84e463373977def5669fc18bd90471f45ee19553
อดิเทพ แบบเหมือน <adithep.baebmuean.2547@gmail.com>
2023-02-27 19:32:27 +0700

    เพิ่มหน้าของ Package Tour และทำให้แสดงผล

--- client/src/components/PackageCard.tsx
+++ client/src/components/PackageCard.tsx
@@ -9,12 +9,12 @@ interface Props {
 function PackageCard(props: Props) {
     const navigate = useNavigate();
     const tour = props.Tour.attributes
-    const img = `http://localhost:1337${tour.cover_image.data.attributes.formats.thumbnail.url}`
+    // const img = `http://localhost:1337${tour.cover_image.data.attributes.formats.thumbnail.url}`
 
     return (
         <div className="card">
             <div className="title-card">{tour.name}</div>
-                <div className="card-image"><img alt="" src={img} className="picture1"/></div>
+                {/* <div className="card-image"><img alt="" src={} className="picture1"/></div> */}
                 <div className="heading">{tour.description}
                 <div className="see-more">
                     <button onClick={() => navigate(`/additional-information-package-tour/${props.Tour.id}`)}>
