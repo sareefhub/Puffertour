c98931c87e1e6422b0ee493c7d9d32b01b988cbc
Sareef Masamaeng <sareef.masamaeng@gmail.com>
2023-03-03 22:17:11 +0700

    Fix userProfile page

--- client/src/pages/userProfile.tsx
+++ client/src/pages/userProfile.tsx
@@ -1,82 +1,45 @@
 import React from 'react'
 import Navbar from '../components/Navbar';
+import { TextField, Grid } from '@mui/material';
 
 import './userProfile.css'
 
 const UserProfile:React.FC = (props) => {
-  return (
-    <div className="user-container">
-    <Navbar/>
-    <div className="main-bodyy">
-    <div className="row gutters-sm">
-      <div className="col-md-4 mb-3">
-        <div className="cardd">
-          <div className="cardd-bodyy">
-            <div className="d-flex flex-column align-items-center text-center">
-              <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Admin" className="rounded-circle" width="150"/>
-              <div className="mt-3">
-                <h4>Sola Jang</h4>
-                <p className="text-secondary mb-1">Full Stack Developer</p>
-                <p className="text-muted font-size-sm">Bay Area, San Francisco, CA</p>
-              </div>
-            </div>
+
+return (
+  <div className="user-container">
+  <Navbar/>
+  <div className="main-bodyy">
+    <div className="cardd">
+          <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Admin" className="rounded-circle" width="150"/>
+          <div className="user-name">
+            <h4>Sola Jang</h4>
           </div>
-        </div>
-          </div>
-          <div className="col-md-8">
-            <div className="cardd mb-3">
-              <div className="cardd-bodyy">
-                <div className="row">
-                  <div className="col-sm-3">
-                    <h6 className="mb-0">Full Name</h6>
-                  </div>
-                  <div className="col-sm-9 text-secondary">
-                    Kenneth Valdez
-                  </div>
-                </div>
-                  <div className="row">
-                    <div className="col-sm-3">
-                      <h6 className="mb-0">Email</h6>
-                    </div>
-                    <div className="col-sm-9 text-secondary">
-                      fip@jukmuh.al
-                    </div>
-                  </div>
-                <div className="row">
-                  <div className="col-sm-3">
-                    <h6 className="mb-0">Phone</h6>
-                  </div>
-                  <div className="col-sm-9 text-secondary">
-                    (239) 816-9029
-                  </div>
-                </div>
-                  <div className="row">
-                    <div className="col-sm-3">
-                      <h6 className="mb-0">Mobile</h6>
-                    </div>
-                    <div className="col-sm-9 text-secondary">
-                      (320) 380-4539
-                    </div>
-                  </div>
-                  <div className="row">
-                    <div className="col-sm-3">
-                      <h6 className="mb-0">Address</h6>
-                    </div>
-                    <div className="col-sm-9 text-secondary">
-                      Bay Area, San Francisco, CA
-                    </div>
-                  </div>
-                <div className="row">
-                  <div className="col-sm-12">
-                    <a className="btn btn-info " target="__blank" href="https://www.bootdey.com/snippets/view/profile-edit-data-and-skills">Edit</a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
+    </div>
+      <div className="cardd mb-3">
+      <Grid item xs={12} sm={6}>
+          <TextField
+            name="username"
+            required
+            fullWidth
+            id="username"
+            label="ชื่อบัญชีผู้ใช้งาน"
+            autoFocus
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            name="email"
+            required
+            fullWidth
+            id="email"
+            label="อีเมล"
+            autoFocus
+          />
+        </Grid>
+    </div>
+  </div>  
   )
 };
 
