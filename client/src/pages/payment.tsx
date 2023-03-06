78ee2b46c1c93f842d767620ecc73f63466b9199
โซล่า ไพบูลย์สมบัติ <solask2546@gmail.com>
2023-03-07 05:02:40 +0700

    create function on payment page

--- client/src/pages/payment.tsx
+++ client/src/pages/payment.tsx
@@ -1,30 +1,83 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
-import Navbar from '../components/Navbar'
-import Swal from 'sweetalert2';
-import './payment.css'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import Swal from "sweetalert2";
+import "./payment.css";
 
-const Payment:React.FC  = (props) => {
-    const navigate = useNavigate();
-    
+const Payment = () => {
+  const navigate = useNavigate();
+  const [selectedFile, setSelectedFile] = useState<File | undefined>(undefined);
+
+  const handleFileSelect = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file && file.size > 2 * 1024 * 1024) {
+      Swal.fire({
+        icon: "error",
+        title: "File size too large",
+        text: "Please select a file that is smaller than 2MB",
+      });
+    } else {
+      setSelectedFile(file);
+    }
+  };
+  
+  const handleImageUpload = async () => {
+    if (!selectedFile) {
+      Swal.fire({
+        icon: "error",
+        title: "No file selected",
+        text: "Please select a file to upload",
+      });
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("image", selectedFile);
+
+    try {
+      const response = await fetch("/api/upload-image", {
+        method: "POST",
+        body: formData,
+      });
+
+      if (response.ok) {
+        navigate("/booking-history");
+      } else {
+        Swal.fire({
+          icon: "error",
+          title: "Error uploading image",
+          text: "Please try again later",
+        });
+      }
+    } catch (error) {
+      Swal.fire({
+        icon: "error",
+        title: "Error uploading image",
+        text: "Please try again later",
+      });
+    }
+  };
   return (
-    <div className="payment-container"> 
-      <Navbar/>
+    <div className="payment-container">
+      <Navbar />
       <div className="payment-container04">
         <form className="payment-form08">
           <svg viewBox="0 0 877.7142857142857 1024" className="payment-icon12">
-            <path d="M877.714 512c0 242.286-196.571 438.857-438.857 438.857s-438.857-196.571-438.857-438.857 196.571-438.857 438.857-438.857 438.857 196.571 438.857 438.857z"></path>
+            <image
+              xlinkHref="./pictures/whitecircle-removebg-preview.png"
+              width="100%"
+              height="100%"
+            />
           </svg>
           <span className="payment-text01">ชำระเงิน</span>
         </form>
-        <form className="payment-form09">
-          <svg viewBox="0 0 1024 1024" className="payment-icon14">
-            <path d="M768 469.333h-512c-47.104 0-85.333 38.229-85.333 85.333s38.229 85.333 85.333 85.333h512c47.104 0 85.333-38.229 85.333-85.333s-38.229-85.333-85.333-85.333z"></path>
-          </svg>
-        </form>
         <form className="payment-form10">
           <svg viewBox="0 0 877.7142857142857 1024" className="payment-icon16">
-            <path d="M877.714 512c0 242.286-196.571 438.857-438.857 438.857s-438.857-196.571-438.857-438.857 196.571-438.857 438.857-438.857 438.857 196.571 438.857 438.857z"></path>
+            <image
+              xlinkHref="./pictures/whitecircle-removebg-preview.png"
+              width="100%"
+              height="100%"
+            />
           </svg>
           <span className="payment-text02">ชำระเงินสำเร็จ</span>
         </form>
@@ -34,62 +87,51 @@ const Payment:React.FC  = (props) => {
           <span className="payment-text03">กรุณาสแกนเพื่อชำระเงิน</span>
           <img
             alt=""
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/QR_code_for_mobile_English_Wikipedia.svg/1024px-QR_code_for_mobile_English_Wikipedia.svg.png"
+            src="./pictures/qrcode.png"
             className="payment-image2"
           />
         </form>
-      <div className="payment-container06">
-        <h1 className="payment-text04">
-          <text>รายละเอียดการโอนเงิน</text>
-        </h1>
-        <text className="promptpay">
-          ธนาคารกสิกรไทย : หมายเลขบัญชี 141-x-xxxxx-0
-        </text>
-        <text className="promptpay">
-          บัญชีที่โอนเงิน : พร้อมเพย์ 093-xxxxx7-0 
-        </text>
-        <text className="promptpay">
-          ชื่อบัญชี บริษัท Puffertour
-        </text>
-        <text className="promptpay">
-        กรุณาแนบไฟล์สลิปการโอนเงิน
-        </text>
-          <input
-            type="file"
-            accept="image/*,.pdf"
-            onChange={async (e) => {
-              const { value: file } = await Swal.fire({
-                title: 'Select image',
-                input: 'file',
-                inputAttributes: {
-                  'accept': 'image/*',
-                  'aria-label': 'Upload your profile picture'
-                }
-              })
-              
-              if (file) {
-                const reader = new FileReader()
-                reader.onload = (e) => {
-                  if (e.target && e.target.result) { // add null check
-                    Swal.fire({
-                      title: 'Your uploaded picture',
-                      imageUrl: e.target.result.toString(),
-                      imageAlt: 'The uploaded picture'
-                    })
-                  }
-                }
-                reader.readAsDataURL(file)
-              }
-            }}
-          />
-          <div className="limit-file">
-            <text>[ ไฟล์ jpg,gif,png,pdf ไม่เกิน2MB ]</text>
+        <div className="payment-container06">
+          <h1 className="payment-text04">
+            <text>รายละเอียดการโอนเงิน</text>
+          </h1>
+          <text className="promptpay">
+            ธนาคารกสิกรไทย : หมายเลขบัญชี 141-x-xxxxx-0
+          </text>
+          <text className="promptpay">
+            บัญชีที่โอนเงิน : พร้อมเพย์ 093-xxxxx7-0
+          </text>
+          <text className="promptpay">ชื่อบัญชี บริษัท Puffertour</text>
+          <form onSubmit={handleImageUpload}>
+            <div className="input-container">
+              <label htmlFor="file-input" className="promptpay">
+                แนบไฟล์สลิปการโอนเงิน
+              </label>
+              <input
+                id="file-input"
+                type="file"
+                accept="image/*,.pdf"
+                onChange={handleFileSelect}
+              />
+            </div>
+            {selectedFile && (
+              <div className="limit-file">
+                <text>[ ไฟล์ jpg, gif, png, pdf ไม่เกิน 2MB ]</text>
+              </div>
+            )}
+            {!selectedFile && (
+              <div className="limit-file">
+                <text>[ กรุณาแนบไฟล์สลิปการโอนเงิน ]</text>
+              </div>
+            )}
+            <button className="button" type="submit" disabled={!selectedFile}>
+              ยืนยันการส่ง
+            </button>
+          </form>
         </div>
-        <button className="button"> ยืนยันการส่ง </button>
       </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Payment
+export default Payment;
