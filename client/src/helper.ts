2d303e055bb7e70bd0972b60bd24df3a6b4e063a
Sareef Masamaeng <sareef.masamaeng@gmail.com>
2023-03-07 13:18:36 +0700

    Show username and email in userProfile

--- client/src/helper.ts
+++ client/src/helper.ts
@@ -1,9 +1,17 @@
 export const storeUser = (data: any) => {
     localStorage.setItem('username', data.user.username);
+    localStorage.setItem('email', data.user.email);
     localStorage.setItem('token', data.jwt);
   };
 export const getUserData = () => {
     const stringfiedUser = localStorage.getItem('username') || ""
+    if (stringfiedUser) {
+        return JSON.parse(stringfiedUser)
+    }
+    return false
+}
+export const getEmailData = () => {
+    const stringfiedUser = localStorage.getItem('email') || ""
     if (stringfiedUser) {
         return JSON.parse(stringfiedUser)
     }
