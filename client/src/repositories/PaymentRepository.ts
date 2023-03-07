84808bee50e5c3028b4f2c484c6b3705f6634840
psu6510110425 <6510110425@email.psu.ac.th>
2023-03-07 14:49:36 +0700

    ระบบที่ยังไม่work

---
+++ client/src/repositories/PaymentRepository.ts
@@ -0,0 +1,48 @@
+import { IRepository } from "./IRepository"
+import Payment from "../models/Payment";
+import ReserveStatus from "../models/Reserve";
+import { getUserData } from "../helper";
+
+const user = getUserData()
+
+export class PaymentRepository implements IRepository<Payment | ReserveStatus>{
+    urlPrefix = "http://localhost:1337/api/payments"
+    token = user.jwt
+
+    async getByUser(): Promise<ReserveStatus[] | null> {
+        const res = await fetch(`${this.urlPrefix}?populate=*&filters[user][$eq]=${user.username}`,{
+            method: "GET",
+            headers: {
+                "Authorization": `Bearer ${this.token}`
+            }
+        })
+        const data = await res.json()
+        return data.data
+    }
+
+    async createPayment(data: Payment): Promise<Payment> {
+        const resp = await fetch(`${this.urlPrefix}`, {
+            method: "POST",
+            headers: {
+                "Authorization": `Bearer ${this.token}`,
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        });
+        const data_res = await resp.json()
+        return data_res;
+    }
+
+    async deletePayment(id: string | number): Promise<void> {
+        const resp = await fetch(`${this.urlPrefix}/${id}`, {
+            method: "DELETE",
+            headers: {
+                "Authorization": `Bearer ${this.token}`,
+            }
+        })
+        const data_res = await resp.json()
+        return data_res
+    }
+
+}
