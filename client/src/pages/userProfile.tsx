cd7d7c1fc54e02960995589f3262472fed78dcb5
Sareef Masamaeng <sareef.masamaeng@gmail.com>
2023-03-08 08:35:28 +0700

    Fix userProfile page

--- client/src/pages/userProfile.tsx
+++ client/src/pages/userProfile.tsx
@@ -10,45 +10,45 @@ const UserProfile:React.FC = (props) => {
   return (
     <div className="user-container">
       <Navbar />
-      <div className="main-bodyy">
-        <div className="cardd">
+      <div className="cardd-container">
+        <div className="cardd1">
           <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Admin" className="rounded-circle" width="150" />
           <div className="user-name">
             <h4>{user.username}</h4>
           </div>
         </div>
-      </div>
-      <div className="cardd mb-3">
-        <form>
-          <Grid container spacing={2}>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                name="username"
-                required
-                fullWidth
-                id="username"
-                label="ชื่อบัญชีผู้ใช้งาน"
-                autoFocus
-                value={user.username}
-                disabled
-              />
+        <div className="cardd2">
+          <div className="text-container">
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  name="username"
+                  required
+                  fullWidth
+                  id="username"
+                  label="ชื่อบัญชีผู้ใช้งาน"
+                  autoFocus
+                  value={user.username}
+                  disabled
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  name="email"
+                  required
+                  fullWidth
+                  id="email"
+                  label="อีเมล"
+                  autoFocus
+                  value={user.email}
+                  disabled
+                />
+              </Grid>
             </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                name="email"
-                required
-                fullWidth
-                id="email"
-                label="อีเมล"
-                autoFocus
-                value={user.email}
-                disabled
-              />
-            </Grid>
-          </Grid>
-        </form>
-      </div>
-    </div>
+          </div>
+        </div>
+      </div>  
+    </div> 
   );
 };
 
