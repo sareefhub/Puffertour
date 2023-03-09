85ecef6d0899a236301ab129a5688f071fb283ff
อดิเทพ แบบเหมือน <adithep.baebmuean.2547@gmail.com>
2023-03-10 02:30:57 +0700

    fix conf

--- client/src/conf.ts
+++ client/src/conf.ts
@@ -2,7 +2,7 @@ const isProd = !(!process.env.NODE_ENV || process.env.NODE_ENV === 'development'
 
 const conf = {
   isProd,
-  apiPrefix: isProd ? 'https://s04x.coe.psu.ac.th/' : 'http://localhost:1337/',
+  apiPrefix: isProd ? 'https://s04x.coe.psu.ac.th' : 'http://localhost:1337',
 }
 
 export default conf;
